b5cbfe637c2e4fcbf61ea9f285ddb87e5a9cc520
Jasmine <jasmine_singh1992@hotmail.com>
2020-03-30 23:35:22 +0000

    ex 4, 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,18 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+totalRevenue = Store.sum(:annual_revenue)
+puts "total rev #{totalRevenue}"
+avergaeRevenue = totalRevenue / Store.count
+puts "average rev #{avergaeRevenue}"
+
+puts Store.where('annual_revenue > ?', 1_000_000).count
