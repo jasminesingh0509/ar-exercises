bd338b1c0c624d9aec665b0dd83a96b40177b3ba
Jasmine <jasmine_singh1992@hotmail.com>
2020-03-30 22:48:19 +0000

    made the data base, ex 1, 2, 3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
-puts "----------"
+puts 'Exercise 2'
+puts '----------'
 
 # Your code goes here ...
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+
+@store1 = Store.find(1)
+
+@store2 = Store.find(2)
+
+@store1.name = 'Victoria'
+@store1.save
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
-puts "Exercise 3"
-puts "----------"
+puts 'Exercise 3'
+puts '----------'
 
 # Your code goes here ...
+
+@store3 = Store.find(3)
+@store3.destroy
+
+count = Store.count
+puts "Store count is #{count}"
